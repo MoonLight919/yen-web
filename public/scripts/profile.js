6670de7602b16fa99c52969f9b3d01d788e8662f
MoonLight919 <igorfujitsu@gmail.com>
2024-03-21 23:03:27 +0200

    feat(): create all pages

---
+++ public/scripts/profile.js
@@ -0,0 +1,21 @@
+$('#alerts-in-ua').hide();
+
+$('#btnradio1').on('change', (e) => {
+    $('#general-information').show();
+    $('#alerts-in-ua').hide();
+});
+
+$('#btnradio2').on('change', (e) => {
+    $('#general-information').hide();
+    $('#alerts-in-ua').show();
+});
+
+$('#btnradio3').on('change', (e) => {
+    $('#general-information').hide();
+    $('#alerts-in-ua').hide();
+});
+
+$('#btnradio4').on('change', (e) => {
+    $('#general-information').hide();
+    $('#alerts-in-ua').hide();
+});
