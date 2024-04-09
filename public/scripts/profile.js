6daaaeeef14694deb0625697d32535f9fdf9e619
MoonLight919 <igorfujitsu@gmail.com>
2024-04-09 20:34:21 +0300

    feat(profile): alerts in ua test notification

--- public/scripts/profile.js
+++ public/scripts/profile.js
@@ -1,6 +1,32 @@
 $('#alerts-in-ua').hide();
 $('#iqair').hide();
 $('#test-phone-number-loading').hide();
+$('#alerts-in-ua-test-loading').hide();
+
+$('#alerts-in-ua-test').on('click', (e) => {
+  $('#alerts-in-ua-test').hide();
+  $('#alerts-in-ua-test-loading').show();
+  $(document).ready(function() {
+    $.ajax({
+      url: 'https://yen-api-7d8c3d9f30f7.herokuapp.com/v1/alerts-in-ua/current-information',
+      type: 'POST',
+      headers: {
+        'Authorization': 'Bearer ' + localStorage.getItem('access_token'),
+      },
+      error: function(xhr) {
+        console.error('Error:', xhr.responseJSON);
+        if(xhr.status === 401) {
+            localStorage.removeItem('access_token');
+            window.location.href = "https://www.yenebezpeka.pp.ua/authorize";
+        }
+      },
+      success: function(data) {
+        $('#alerts-in-ua-test').show();
+        $('#alerts-in-ua-test-loading').hide();
+      }
+    });
+  });    
+});
 
 $('#btnradio1').on('change', (e) => {
     $('#general-information').show();
@@ -27,26 +53,26 @@ $('#btnradio4').on('change', (e) => {
 });
 
 $('#test-phone-number').on('click', (e) => {
-    $('#test-phone-number').hide();
-    $('#test-phone-number-loading').show();
-    $(document).ready(function() {
-        $.ajax({
-          url: 'https://yen-api-7d8c3d9f30f7.herokuapp.com/v1/twilio/test',
-          type: 'POST',
-          headers: {
-            'Authorization': 'Bearer ' + localStorage.getItem('access_token'),
-          },
-          error: function(xhr) {
-            console.error('Error:', xhr.responseJSON);
-            if(xhr.status === 401) {
-                localStorage.removeItem('access_token');
-                window.location.href = "https://www.yenebezpeka.pp.ua/authorize";
-            }
-          },
-          success: function(data) {
-            $('#test-phone-number').show();
-            $('#test-phone-number-loading').hide();
-          }
-        });
-      });    
-})
+  $('#test-phone-number').hide();
+  $('#test-phone-number-loading').show();
+  $(document).ready(function() {
+    $.ajax({
+      url: 'https://yen-api-7d8c3d9f30f7.herokuapp.com/v1/twilio/test',
+      type: 'POST',
+      headers: {
+        'Authorization': 'Bearer ' + localStorage.getItem('access_token'),
+      },
+      error: function(xhr) {
+        console.error('Error:', xhr.responseJSON);
+        if(xhr.status === 401) {
+            localStorage.removeItem('access_token');
+            window.location.href = "https://www.yenebezpeka.pp.ua/authorize";
+        }
+      },
+      success: function(data) {
+        $('#test-phone-number').show();
+        $('#test-phone-number-loading').hide();
+      }
+    });
+  });    
+});
