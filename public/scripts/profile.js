ef66812eccb31e1960a6337198d7b85e4e2f7738
MoonLight919 <igorfujitsu@gmail.com>
2024-04-16 18:39:03 +0300

    feat(profile): all current information endpoints

--- public/scripts/profile.js
+++ public/scripts/profile.js
@@ -3,13 +3,144 @@ $('#iqair').hide();
 $('#test-phone-number-loading').hide();
 $('#alerts-in-ua-test-loading').hide();
 $('#air-quality-test-loading').hide();
+$('#air-temperature-test-loading').hide();
+$('#atmospheric-pressure-test-loading').hide();
+$('#humidity-test-loading').hide();
+$('#wind-speed-test-loading').hide();
+$('#weather-test-loading').hide();
+
+$('#weather-test').on('click', (e) => {
+  $('#weather-test').hide();
+  $('#weather-test-loading').show();
+  $(document).ready(function() {
+    $.ajax({
+      url: 'https://yen-api-7d8c3d9f30f7.herokuapp.com/v1/iqair/current-information/weather',
+      type: 'POST',
+      headers: {
+        'Authorization': 'Bearer ' + localStorage.getItem('access_token'),
+      },
+      error: function(xhr) {
+        console.error('Error:', xhr.responseJSON);
+        if(xhr.status === 401) {
+            localStorage.removeItem('access_token');
+            window.location.href = "https://www.yenebezpeka.pp.ua/authorize";
+        }
+      },
+      success: function(data) {
+        $('#weather-test').show();
+        $('#weather-test-loading').hide();
+      }
+    });
+  });    
+});
+
+$('#wind-speed-test').on('click', (e) => {
+  $('#wind-speed-test').hide();
+  $('#wind-speed-test-loading').show();
+  $(document).ready(function() {
+    $.ajax({
+      url: 'https://yen-api-7d8c3d9f30f7.herokuapp.com/v1/iqair/current-information/wind-speed',
+      type: 'POST',
+      headers: {
+        'Authorization': 'Bearer ' + localStorage.getItem('access_token'),
+      },
+      error: function(xhr) {
+        console.error('Error:', xhr.responseJSON);
+        if(xhr.status === 401) {
+            localStorage.removeItem('access_token');
+            window.location.href = "https://www.yenebezpeka.pp.ua/authorize";
+        }
+      },
+      success: function(data) {
+        $('#wind-speed-test').show();
+        $('#wind-speed-test-loading').hide();
+      }
+    });
+  });    
+});
+
+
+$('#humidity-test').on('click', (e) => {
+  $('#humidity-test').hide();
+  $('#humidity-test-loading').show();
+  $(document).ready(function() {
+    $.ajax({
+      url: 'https://yen-api-7d8c3d9f30f7.herokuapp.com/v1/iqair/current-information/humidity',
+      type: 'POST',
+      headers: {
+        'Authorization': 'Bearer ' + localStorage.getItem('access_token'),
+      },
+      error: function(xhr) {
+        console.error('Error:', xhr.responseJSON);
+        if(xhr.status === 401) {
+            localStorage.removeItem('access_token');
+            window.location.href = "https://www.yenebezpeka.pp.ua/authorize";
+        }
+      },
+      success: function(data) {
+        $('#humidity-test').show();
+        $('#humidity-test-loading').hide();
+      }
+    });
+  });    
+});
+
+$('#atmospheric-pressure-test').on('click', (e) => {
+  $('#atmospheric-pressure-test').hide();
+  $('#atmospheric-pressure-test-loading').show();
+  $(document).ready(function() {
+    $.ajax({
+      url: 'https://yen-api-7d8c3d9f30f7.herokuapp.com/v1/iqair/current-information/atmospheric-pressure',
+      type: 'POST',
+      headers: {
+        'Authorization': 'Bearer ' + localStorage.getItem('access_token'),
+      },
+      error: function(xhr) {
+        console.error('Error:', xhr.responseJSON);
+        if(xhr.status === 401) {
+            localStorage.removeItem('access_token');
+            window.location.href = "https://www.yenebezpeka.pp.ua/authorize";
+        }
+      },
+      success: function(data) {
+        $('#atmospheric-pressure-test').show();
+        $('#atmospheric-pressure-test-loading').hide();
+      }
+    });
+  });    
+});
+
+$('#air-temperature-test').on('click', (e) => {
+  $('#air-temperature-test').hide();
+  $('#air-temperature-test-loading').show();
+  $(document).ready(function() {
+    $.ajax({
+      url: 'https://yen-api-7d8c3d9f30f7.herokuapp.com/v1/iqair/current-information/air-temperature',
+      type: 'POST',
+      headers: {
+        'Authorization': 'Bearer ' + localStorage.getItem('access_token'),
+      },
+      error: function(xhr) {
+        console.error('Error:', xhr.responseJSON);
+        if(xhr.status === 401) {
+            localStorage.removeItem('access_token');
+            window.location.href = "https://www.yenebezpeka.pp.ua/authorize";
+        }
+      },
+      success: function(data) {
+        $('#air-temperature-test').show();
+        $('#air-temperature-test-loading').hide();
+      }
+    });
+  });    
+});
 
 $('#air-quality-test').on('click', (e) => {
   $('#air-quality-test').hide();
   $('#air-quality-test-loading').show();
   $(document).ready(function() {
     $.ajax({
-      url: 'https://yen-api-7d8c3d9f30f7.herokuapp.com/v1/iqair/air-quality/current-information',
+      url: 'https://yen-api-7d8c3d9f30f7.herokuapp.com/v1/iqair/current-information/air-quality',
       type: 'POST',
       headers: {
         'Authorization': 'Bearer ' + localStorage.getItem('access_token'),
