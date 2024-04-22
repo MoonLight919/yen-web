1e2a23eb7ee4fec33a57edecca05d817dd0042c7
MoonLight919 <igorfujitsu@gmail.com>
2024-04-22 20:36:39 +0300

    fix(profile): add configuration to every metric

--- public/scripts/token-validation.js
+++ public/scripts/token-validation.js
@@ -1,19 +1,19 @@
-const access_token = localStorage.getItem('access_token');
-if(!access_token){
-    window.location.href = "https://www.yenebezpeka.pp.ua/authorize";
-}
+// const access_token = localStorage.getItem('access_token');
+// if(!access_token){
+//     window.location.href = "https://www.yenebezpeka.pp.ua/authorize";
+// }
 
-$(document).ready(function() {
-    $.ajax({
-      url: 'https://yen-api-7d8c3d9f30f7.herokuapp.com/v1/auth/validate-token',
-      type: 'POST',
-      headers: {
-        'X-API-Key': 'hIy56nPdlOFYLFlqkGDWt1AAgMWtS2cW',
-        'Authorization': 'Bearer ' + access_token,
-      },
-      error: function(xhr, status, error) {
-        localStorage.removeItem('access_token');
-        window.location.href = "https://www.yenebezpeka.pp.ua/authorize";
-      }
-    });
-  });
+// $(document).ready(function() {
+//     $.ajax({
+//       url: 'https://yen-api-7d8c3d9f30f7.herokuapp.com/v1/auth/validate-token',
+//       type: 'POST',
+//       headers: {
+//         'X-API-Key': 'hIy56nPdlOFYLFlqkGDWt1AAgMWtS2cW',
+//         'Authorization': 'Bearer ' + access_token,
+//       },
+//       error: function(xhr, status, error) {
+//         localStorage.removeItem('access_token');
+//         window.location.href = "https://www.yenebezpeka.pp.ua/authorize";
+//       }
+//     });
+//   });
