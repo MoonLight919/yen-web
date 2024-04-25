c757183389d74d44b883392da906219b97a91fb6
MoonLight919 <igorfujitsu@gmail.com>
2024-04-25 12:15:27 +0300

    feat(profile): notification details update

--- public/scripts/profile.js
+++ public/scripts/profile.js
@@ -1,6 +1,6 @@
 $('#alerts-in-ua').hide();
 $('#iqair').hide();
-$('#save-eco-bot').hide();
+$('#radiation').hide();
 $('#test-phone-number-loading').hide();
 $('#alerts-in-ua-test-loading').hide();
 $('#air-quality-test-loading').hide();
@@ -9,235 +9,157 @@ $('#atmospheric-pressure-test-loading').hide();
 $('#humidity-test-loading').hide();
 $('#wind-speed-test-loading').hide();
 $('#weather-test-loading').hide();
-$('#save-eco-bot-test-loading').hide();
+$('#radiation-test-loading').hide();
+$('#save-alerts-in-ua-loading').hide();
+$('#save-air-quality-loading').hide();
+$('#save-air-temperature-loading').hide();
+$('#save-atmospheric-pressure-loading').hide();
+$('#save-humidity-loading').hide();
+$('#save-wind-speed-loading').hide();
+$('#save-weather-loading').hide();
+$('#save-radiation-loading').hide();
 
-$('#save-eco-bot-test').on('click', (e) => {
-  $('#save-eco-bot-test').hide();
-  $('#save-eco-bot-test-loading').show();
-  $(document).ready(function() {
-    $.ajax({
-      url: 'https://yen-api-7d8c3d9f30f7.herokuapp.com/v1/save-eco-bot/current-information',
-      type: 'POST',
-      headers: {
-        'Authorization': 'Bearer ' + localStorage.getItem('access_token'),
-      },
-      error: function(xhr) {
-        console.error('Error:', xhr.responseJSON);
-        if(xhr.status === 401) {
-            localStorage.removeItem('access_token');
-            window.location.href = "https://www.yenebezpeka.pp.ua/authorize";
+const categoriesAndURLs = {
+  'weather': '/iqair/current-information/weather',
+  'wind-speed': '/iqair/current-information/wind-speed',
+  'humidity': '/iqair/current-information/humidity',
+  'atmospheric-pressure': '/iqair/current-information/atmospheric-pressure',
+  'air-temperature': '/iqair/current-information/air-temperature',
+  'air-quality': '/iqair/current-information/air-quality',
+  'radiation': '/save-eco-bot/current-information',
+  'alerts-in-ua': '/alerts-in-ua/current-information',
+};
+
+Object.keys(categoriesAndURLs).forEach(category => {
+  $(`#${category}-test`).on('click', (e) => {
+    $(`#${category}-test`).hide();
+    $(`#${category}-test-loading`).show();
+    $(document).ready(function() {
+      $.ajax({
+        url: `https://yen-api-7d8c3d9f30f7.herokuapp.com/v1${categoriesAndURLs[category]}`,
+        type: 'POST',
+        headers: {
+          'Authorization': 'Bearer ' + localStorage.getItem('access_token'),
+        },
+        error: function(xhr) {
+          console.error('Error:', xhr.responseJSON);
+          if(xhr.status === 401) {
+              localStorage.removeItem('access_token');
+              window.location.href = "https://www.yenebezpeka.pp.ua/authorize";
+          }
+        },
+        success: function(data) {
+          $(`#${category}-test`).show();
+          $(`#${category}-test-loading`).hide();
         }
-      },
-      success: function(data) {
-        $('#save-eco-bot-test').show();
-        $('#save-eco-bot-test-loading').hide();
-      }
+      });
+    });    
+  });
+
+  $(`#save-${category}`).on('click', (e) => {
+    $(`#save-${category}`).hide();
+    $(`#save-${category}-loading`).show();
+    const payload = {
+      type: category,
+      default_location: $(`#default-location-${category}`).prop('checked') ?? false,
+      active: $(`#${category}-notification`).prop('checked'),
+    };
+    if($(`#${category}-notification-trigger-value`)) {
+      payload.trigger_value = $(`#${category}-notification-trigger-value`).val()
+    }
+    let url = 'https://yen-api-7d8c3d9f30f7.herokuapp.com/v1/notification-details';
+    let type = 'POST';
+    const detail = notificationDetails.find(detail => detail.type === category);
+    if(detail) {
+      url += `/${detail.id}`;
+      type = 'PATCH'
+    }
+    $(document).ready(function() {
+      $.ajax({
+        url,
+        type,
+        contentType: 'application/json',
+        headers: {
+          'Authorization': 'Bearer ' + localStorage.getItem('access_token'),
+        },
+        data: JSON.stringify(payload),
+        error: function(xhr) {
+          console.error('Error:', xhr.responseJSON);
+          if(xhr.status === 401) {
+              localStorage.removeItem('access_token');
+              window.location.href = "https://www.yenebezpeka.pp.ua/authorize";
+          }
+        },
+        success: function(data) {
+          $(`#save-${category}`).show();
+          $(`#save-${category}-loading`).hide();
+        }
+      });
     });
-  });    
+  });
 });
 
-$('#weather-test').on('click', (e) => {
-  $('#weather-test').hide();
-  $('#weather-test-loading').show();
-  $(document).ready(function() {
-    $.ajax({
-      url: 'https://yen-api-7d8c3d9f30f7.herokuapp.com/v1/iqair/current-information/weather',
-      type: 'POST',
-      headers: {
-        'Authorization': 'Bearer ' + localStorage.getItem('access_token'),
-      },
-      error: function(xhr) {
-        console.error('Error:', xhr.responseJSON);
-        if(xhr.status === 401) {
-            localStorage.removeItem('access_token');
-            window.location.href = "https://www.yenebezpeka.pp.ua/authorize";
-        }
-      },
-      success: function(data) {
-        $('#weather-test').show();
-        $('#weather-test-loading').hide();
-      }
-    });
-  });    
-});
+let notificationDetails;
 
-$('#wind-speed-test').on('click', (e) => {
-  $('#wind-speed-test').hide();
-  $('#wind-speed-test-loading').show();
-  $(document).ready(function() {
-    $.ajax({
-      url: 'https://yen-api-7d8c3d9f30f7.herokuapp.com/v1/iqair/current-information/wind-speed',
-      type: 'POST',
-      headers: {
-        'Authorization': 'Bearer ' + localStorage.getItem('access_token'),
-      },
-      error: function(xhr) {
-        console.error('Error:', xhr.responseJSON);
-        if(xhr.status === 401) {
-            localStorage.removeItem('access_token');
-            window.location.href = "https://www.yenebezpeka.pp.ua/authorize";
-        }
-      },
-      success: function(data) {
-        $('#wind-speed-test').show();
-        $('#wind-speed-test-loading').hide();
+$(document).ready(function() {
+  $.ajax({
+    url: 'https://yen-api-7d8c3d9f30f7.herokuapp.com/v1/notification-details',
+    type: 'GET',
+    contentType: 'application/json',
+    headers: {
+      'Authorization': 'Bearer ' + localStorage.getItem('access_token'),
+    },
+    error: function(xhr) {
+      console.error('Error:', xhr.responseJSON);
+      if(xhr.status === 401) {
+          localStorage.removeItem('access_token');
+          window.location.href = "https://www.yenebezpeka.pp.ua/authorize";
       }
-    });
-  });    
-});
-
-
-$('#humidity-test').on('click', (e) => {
-  $('#humidity-test').hide();
-  $('#humidity-test-loading').show();
-  $(document).ready(function() {
-    $.ajax({
-      url: 'https://yen-api-7d8c3d9f30f7.herokuapp.com/v1/iqair/current-information/humidity',
-      type: 'POST',
-      headers: {
-        'Authorization': 'Bearer ' + localStorage.getItem('access_token'),
-      },
-      error: function(xhr) {
-        console.error('Error:', xhr.responseJSON);
-        if(xhr.status === 401) {
-            localStorage.removeItem('access_token');
-            window.location.href = "https://www.yenebezpeka.pp.ua/authorize";
+    },
+    success: function(response) {
+      notificationDetails = response.data;
+      
+      Object.keys(categoriesAndURLs).forEach(category => {
+        const detail = notificationDetails.find(detail => detail.type === category);
+        if(detail) {
+          $(`#${category}-notification`).prop('checked', detail.active);
+          $(`#current-location-${category}`).prop('checked', !detail.default_location);
+          $(`#default-location-${category}`).prop('checked', detail.default_location);
+          if($(`#${category}-notification-trigger-value`)){
+            $(`#${category}-notification-trigger-value`).val(detail.trigger_value);
+          }
         }
-      },
-      success: function(data) {
-        $('#humidity-test').show();
-        $('#humidity-test-loading').hide();
-      }
-    });
-  });    
-});
-
-$('#atmospheric-pressure-test').on('click', (e) => {
-  $('#atmospheric-pressure-test').hide();
-  $('#atmospheric-pressure-test-loading').show();
-  $(document).ready(function() {
-    $.ajax({
-      url: 'https://yen-api-7d8c3d9f30f7.herokuapp.com/v1/iqair/current-information/atmospheric-pressure',
-      type: 'POST',
-      headers: {
-        'Authorization': 'Bearer ' + localStorage.getItem('access_token'),
-      },
-      error: function(xhr) {
-        console.error('Error:', xhr.responseJSON);
-        if(xhr.status === 401) {
-            localStorage.removeItem('access_token');
-            window.location.href = "https://www.yenebezpeka.pp.ua/authorize";
-        }
-      },
-      success: function(data) {
-        $('#atmospheric-pressure-test').show();
-        $('#atmospheric-pressure-test-loading').hide();
-      }
-    });
-  });    
-});
-
-$('#air-temperature-test').on('click', (e) => {
-  $('#air-temperature-test').hide();
-  $('#air-temperature-test-loading').show();
-  $(document).ready(function() {
-    $.ajax({
-      url: 'https://yen-api-7d8c3d9f30f7.herokuapp.com/v1/iqair/current-information/air-temperature',
-      type: 'POST',
-      headers: {
-        'Authorization': 'Bearer ' + localStorage.getItem('access_token'),
-      },
-      error: function(xhr) {
-        console.error('Error:', xhr.responseJSON);
-        if(xhr.status === 401) {
-            localStorage.removeItem('access_token');
-            window.location.href = "https://www.yenebezpeka.pp.ua/authorize";
-        }
-      },
-      success: function(data) {
-        $('#air-temperature-test').show();
-        $('#air-temperature-test-loading').hide();
-      }
-    });
-  });    
-});
-
-$('#air-quality-test').on('click', (e) => {
-  $('#air-quality-test').hide();
-  $('#air-quality-test-loading').show();
-  $(document).ready(function() {
-    $.ajax({
-      url: 'https://yen-api-7d8c3d9f30f7.herokuapp.com/v1/iqair/current-information/air-quality',
-      type: 'POST',
-      headers: {
-        'Authorization': 'Bearer ' + localStorage.getItem('access_token'),
-      },
-      error: function(xhr) {
-        console.error('Error:', xhr.responseJSON);
-        if(xhr.status === 401) {
-            localStorage.removeItem('access_token');
-            window.location.href = "https://www.yenebezpeka.pp.ua/authorize";
-        }
-      },
-      success: function(data) {
-        $('#air-quality-test').show();
-        $('#air-quality-test-loading').hide();
-      }
-    });
-  });    
-});
-
-$('#alerts-in-ua-test').on('click', (e) => {
-  $('#alerts-in-ua-test').hide();
-  $('#alerts-in-ua-test-loading').show();
-  $(document).ready(function() {
-    $.ajax({
-      url: 'https://yen-api-7d8c3d9f30f7.herokuapp.com/v1/alerts-in-ua/current-information',
-      type: 'POST',
-      headers: {
-        'Authorization': 'Bearer ' + localStorage.getItem('access_token'),
-      },
-      error: function(xhr) {
-        console.error('Error:', xhr.responseJSON);
-        if(xhr.status === 401) {
-            localStorage.removeItem('access_token');
-            window.location.href = "https://www.yenebezpeka.pp.ua/authorize";
-        }
-      },
-      success: function(data) {
-        $('#alerts-in-ua-test').show();
-        $('#alerts-in-ua-test-loading').hide();
-      }
-    });
-  });    
+      });
+    }
+  });
 });
 
 $('#btnradio1').on('change', (e) => {
     $('#general-information').show();
     $('#alerts-in-ua').hide();
     $('#iqair').hide();
-    $('#save-eco-bot').hide();
+    $('#radiation').hide();
 });
 
 $('#btnradio2').on('change', (e) => {
     $('#general-information').hide();
     $('#alerts-in-ua').show();
     $('#iqair').hide();
-    $('#save-eco-bot').hide();
+    $('#radiation').hide();
 });
 
 $('#btnradio3').on('change', (e) => {
     $('#general-information').hide();
     $('#alerts-in-ua').hide();
     $('#iqair').show();
-    $('#save-eco-bot').hide();
+    $('#radiation').hide();
 });
 
 $('#btnradio4').on('change', (e) => {
     $('#general-information').hide();
     $('#alerts-in-ua').hide();
     $('#iqair').hide();
-    $('#save-eco-bot').show();
+    $('#radiation').show();
 });
 
 $('#test-phone-number').on('click', (e) => {
